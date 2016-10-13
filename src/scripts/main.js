2fc34e29e2e0ca3a868b5f1ba236a440bfcd879c
CAN <canvolcano@foxmail.com>
2016-10-13 18:18:07 +0800

    Router

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,6 +4,8 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var Hello = require('./components/helloWorld.js');
+import { Router, Route, Link, browserHistory, IndexRoute } from 'react-router'
+
 
 ReactDOM.render(
     <Hello name="World" />,
